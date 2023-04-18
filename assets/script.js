1a0f9bd476379462f63af70d0f1bbf1e2e9f209a
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-18 14:21:10 -0400

    Getting time slots to be lined up & responsive

--- assets/script.js
+++ assets/script.js
@@ -33,24 +33,24 @@ $("#hour-5").attr("data-time", dayjs("5:00 pm", "h:mm a").format("HH"));
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  saveBtn.on("click", function () {
-    var rowHour = $(this).attr()
-  })
+  // saveBtn.on("click", function () {
+  //   var rowHour = $(this).attr()
+  // })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   for (var i = 0; i <= 12; i++) {
-    var inputHour = $("#" + i + "time-row").attr("data-time");
+    var inputHourInt = $("#" + i + "time-row").attr("data-time");
 
     if (currentHourInt === inputHourInt) {
-      $("#" + i + "Row").addClass("present");
+      $("#" + i + "time-row").addClass("present");
     if (currentHourInt > inputHourInt) {
-      $("#" + i + "Row").addClass("past");
+      $("#" + i + "time-row").addClass("past");
     }
     if (currentHourInt < inputHourInt) {
-      $("#" + i + "Row").addClass("future");
+      $("#" + i + "time-row").addClass("future");
     }
   }}
   //
